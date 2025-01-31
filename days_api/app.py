c70db1250b55f0417a29821dce9b8ee81126e496
AntuFTW <antarikshpatel2002@gmail.com>
2025-01-31 17:14:03 +0000

    a

--- days_api/app.py
+++ days_api/app.py
@@ -14,6 +14,11 @@ app_history = []
 app = Flask(__name__)
 
 
+def clear_history():
+    """Clears the app history."""
+    app_history.clear()
+
+
 def add_to_history(current_request):
     """Adds a route to the app history."""
     app_history.append({
@@ -30,10 +35,111 @@ def index():
 
 
 @app.route("/between", methods=['POST'])
-def post_between():
+def between():
+    """Between route function"""
+    # ERROR HANDLE BELLOW
     data = request.json
-    print(data)
-    return data
+    if list(data.keys()) != ['first', 'last']:
+        return {
+            "error": "Missing required data."
+        }, 400
+
+    first = data.get('first')
+    last = data.get('last')
+    # ERROR HANDLE ABOVE
+    try:
+        first = convert_to_datetime(first)
+        last = convert_to_datetime(last)
+        response_json = {
+            "days": get_days_between(first, last)
+        }
+
+        add_to_history(request)
+        return response_json, 200
+    except (TypeError, ValueError):
+        return {
+            "error": "Unable to convert value to datetime."
+        }, 400
+
+
+@app.route("/weekday", methods=['POST'])
+def weekday():
+    """weekday route function"""
+    data = request.json
+    date_in = data.get('date')
+    if list(data.keys()) != ['date']:
+        return {
+            "error": "Missing required data."
+        }, 400
+    try:
+        date_in_datetime = convert_to_datetime(date_in)
+        weekday_in = get_day_of_week_on(date_in_datetime)
+        response_json = {
+            "weekday": weekday_in
+        }
+
+        add_to_history(request)
+        return response_json, 200
+    except (TypeError, ValueError):
+        return {
+            "error": "Unable to convert value to datetime."
+        }, 400
+
+
+@app.route("/history", methods=['GET', 'DELETE'])
+def history():
+    """History route function"""
+    if request.method == 'GET':
+        args = request.args.to_dict()
+        number = args.get('number')
+        if number is None:
+            number = '5'
+        if not number.isdigit():
+            return {
+                "error": "Number must be an integer between 1 and 20."
+            }, 400
+        number = int(number)
+        if number < 1 or number > 20:
+            return {
+                "error": "Number must be an integer between 1 and 20."
+            }, 400
+        # complete error handle above
+        add_to_history(request)
+        start_i = len(app_history) - number
+        print(start_i)
+        print(len(app_history))
+        if start_i <= 0:
+            start_i = 0
+        response_json = app_history[start_i:]
+        response_json = response_json[::-1]
+        return response_json, 200
+
+    if request.method == 'DELETE':
+        clear_history()
+        # add_to_history(request)
+        return {
+            "status": "History cleared"
+        }, 200
+
+
+@app.route("/current_age", methods=['GET'])
+def current_age():
+    """Current_age route function"""
+    args = request.args.to_dict()
+    date = args.get('date')
+    if date is None:
+        return {
+            "error": "Date parameter is required."
+        }, 400
+    try:
+        age = get_current_age(date)
+    except TypeError:
+        return {
+            "error": "Value for data parameter is invalid."
+        }, 400
+    return {
+        "current_age": age
+    }, 200
 
 
 if __name__ == "__main__":
