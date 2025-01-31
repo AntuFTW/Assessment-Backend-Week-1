f2cdefef0aa2697bfdd593fcc39a6b73959cf5f1
AntuFTW <antarikshpatel2002@gmail.com>
2025-01-31 15:15:37 +0000

    functions finished check tests again

--- days_api/date_functions.py
+++ days_api/date_functions.py
@@ -1,9 +1,11 @@
+
 """Functions for working with dates."""
 
 from datetime import datetime, date
 
 
 def convert_to_datetime(date_val: str) -> datetime:
+    """Convert string to datetime obj"""
     try:
         return datetime.strptime(date_val, "%d.%m.%Y")
     except ValueError:
@@ -11,6 +13,7 @@ def convert_to_datetime(date_val: str) -> datetime:
 
 
 def get_days_between(first: datetime, last: datetime) -> int:
+    """Get the number of days between two datetime objects"""
     try:
         diff = last - first
         return diff.days
@@ -19,12 +22,27 @@ def get_days_between(first: datetime, last: datetime) -> int:
 
 
 def get_day_of_week_on(date_val: datetime) -> str:
-    pass
+    """Get the day of week the input obj is on"""
+    try:
+        return date_val.strftime("%A")
+    except AttributeError:
+        raise TypeError("Datetime required.")
 
 
 def get_current_age(birthdate: date) -> int:
-    pass
+    """Get the age of someone with input birthdate"""
+    if not isinstance(birthdate, date):
+        raise TypeError("Date required.")
+
+    today = datetime.now()
+    year_diff = today.year - birthdate.year
+    if today.month < birthdate.month:
+        year_diff -= 1
+    elif today.month == birthdate.month and today.day < birthdate.day:
+        year_diff -= 1
+    return year_diff
 
 
 if __name__ == '__main__':
-    get_days_between('31.02.2013', '17:10:2002')
+    print(get_day_of_week_on(datetime.now()))
+    print(get_current_age())
